1c7bbda5c721eec9b6b135d17421bc719dfb9a3e
jacksonmeade <jacksonmeade@outlook.com>
2023-02-04 17:34:13 -0500

    close for maintenance and add nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 500) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
+
+
+/*
 const menu = document.querySelector('#toggle');  
 const menuItems = document.querySelector('#overlay');  
 const menuContainer = document.querySelector('.menu-container');  
@@ -13,3 +27,4 @@ function toggleMenu(e) {
 }
 
 menu.addEventListener('click', toggleMenu, false);
+*/
